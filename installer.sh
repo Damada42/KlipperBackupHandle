c965ddb2de077a367b624cc454bee9481f60e589
markus <markus.heinz@gmail.com>
2022-05-25 17:20:01 +0200

    clear code

--- installer.sh
+++ installer.sh
@@ -5,16 +5,16 @@ nasbackupfolder="$HOME/shares/klipperbackup"
 logfilesbackupfolder="$HOME/klipper_config/Backuplogs"
 variablefilename='backupvariables.ini'
 echo "########################################################################"
-echo "Please enter working path for backupfolder to create in HOME path: "
+echo "Please enter working path for backupfolder to create in '$HOME/klipper_config' path: "
 echo "If it is empty it use the default path 'backuphandle'. "
 read inputworking
 
 if [ ! -z "$inputworking" ] ; then
     backupfolder=$inputworking
 fi
-echo "The working path are: $HOME/$backupfolder"
-mkdir $HOME/$backupfolder  >/dev/null 2>&1
-filename="$HOME/$backupfolder/$variablefilename"
+echo "The working path are: $HOME/klipper_config/$backupfolder"
+mkdir $HOME/klipper_config/$backupfolder  >/dev/null 2>&1
+filename="$HOME/klipper_config/$backupfolder/$variablefilename"
 echo "########################################################################"
 echo "Please enter USB path to creat it in 'media' folder: "
 echo "If it is empty it use the default path 'usbstickdata'. "
@@ -41,10 +41,13 @@ echo "Please enter Logfile path to creat it in '$HOME/klipper_config' folder: "
 echo "If it is empty it use the default path 'Backuplogs'. "
 read inputlogfiles
 if [ ! -z "$inputnas" ] ; then
-    logfilesbackupfolder=$HOME/klipper_config/Backuplogs
+    logfilesbackupfolder=$HOME/klipper_config/$inputlogfiles
 fi
 echo "The NAS path are: $logfilesbackupfolder"
 mkdir $logfilesbackupfolder >/dev/null 2>&1
+touch $logfilesbackupfolder/crazyfile.log >/dev/null 2>&1
+touch $logfilesbackupfolder/crazyfile1.log >/dev/null 2>&1
+
 echo "########################################################################"
 echo "#######  now it run the rest from the instalation please wait!  ########"
 echo "########################################################################"
